667fcbf0f144fd47d25e70795b7163bde099bc27
melonegoist <408517@niuitmo.ru>
2025-07-22 11:12:07 +0300

    initial commit

---
+++ src/test/java/edu/t1/tokenbasedauth/TokenBasedAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.t1.tokenbasedauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokenBasedAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
